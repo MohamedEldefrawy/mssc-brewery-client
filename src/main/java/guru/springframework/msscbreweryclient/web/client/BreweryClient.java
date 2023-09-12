c1484bf06e1fe730ed1bbe80bd8f9da314d94ae1
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2023-09-12 15:52:56 +0200

    Create BreweryClient

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+  private String apiHost;
+  public final String BEER_PATH_V1 = "/api/v1/beer";
+  private final RestTemplate restTemplate;
+
+  public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+    this.restTemplate = restTemplateBuilder.build();
+  }
+
+  public BeerDto getBeerById(UUID id) {
+    return restTemplate.getForObject(apiHost + BEER_PATH_V1 + id.toString(), BeerDto.class);
+  }
+
+  public void setApiHost(String apiHost) {
+    this.apiHost = apiHost;
+  }
+}
